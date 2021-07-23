2d6c5f99194258bb834578ab5cdf9b9523f9d130
Juan Carlos Bautista <jcba_20@hotmail.com>
2021-07-22 21:05:08 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,20 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Juanes'));
+bands.addBand( new Band('Mana'));
+bands.addBand( new Band('Gian Marcos'));
+bands.addBand( new Band('Cafe tacuba'));
 
 //Mensajes de sockets
 io.on('connection', client => {
     console.log("cliente conectado")
+
+    client.emit('active-bands', bands.getBand());
+
     client.on('disconnect', () => {
         console.log("cliente desconectado");
     });
@@ -11,6 +23,29 @@ io.on('connection', client => {
         console.log("mensaje: ",payload);
     });
 
-    client.emit('mensaje', {mensaje: "nuevo mensaje desde el server"});
+    client.on('vote-band', (payload) =>{
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBand());
+    });
+
+    client.on('add-band', (payload) =>{
+        //console.log(payload);
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBand());
+    });
+
+    client.on('delete-band', (payload) =>{
+        //console.log(payload);
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBand());
+    });
+
+    // client.emit('mensaje', {mensaje: "nuevo mensaje desde el server"});
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     //io.emit('nuevo-mensaje', payload);  //emite a todos
+    //     client.broadcast.emit('nuevo-mensaje', payload);  //emite a todos menos el que lo emitió
+    // });
 
 });
